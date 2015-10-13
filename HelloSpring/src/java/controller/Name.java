f9aeb97963b6d0e234d62710c0c36c9ebd8c49e3
LabGSG <LabGSG@LABGSG-PC22.mshome.net>
2015-10-13 09:25:34 +0700

    coba-coba

---
+++ HelloSpring/src/java/controller/Name.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author LabGSG
+ */
+public class Name {
+    private String value;
+    private String pass;
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+    
+}
---
+++ HelloSpring/src/java/service/HelloService.java
@@ -0,0 +1,17 @@
+package service;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LabGSG
+ */
+public class HelloService {
+    public static String sayHello(String name){
+        return "Hello " + name + "Anda berhasil login";
+    }
+}
